42e7642c8cddaf0528df8c39738406442c49f3ae
Lime Icing <emil577b@stud.kea.dk>
2018-09-28 09:18:52 +0200

    added fields to Server

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,15 @@
+import javax.xml.crypto.Data;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Server {
     List<String> users = new ArrayList<>();
+    DatagramSocket receivingSocket;
+    DatagramSocket sendingSocket;
+    DatagramPacket receivingPacket;
+    DatagramPacket sendingPacket;
+    byte[] sendData = new byte[1024];
+    byte[] receiveData = new byte[1024];
 }
