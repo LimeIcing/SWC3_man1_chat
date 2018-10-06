a21e86bec42b1dcf0d6e4742f9d0fc9ed423fc65
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-10-06 11:36:21 +0200

    comments in user class

--- src/User.java
+++ src/User.java
@@ -1,12 +1,25 @@
 import java.net.InetAddress;
 import java.util.Calendar;
 
+/**
+ * @see        User this class is what an user looks like in our system
+ * username    The username of the client.
+ * IP          The ip of the client.
+ * isTimedOut  boolean to determent whether or not the client is online.
+ * calender    To determent the clients time
+ */
+
 public class User {
     private String username;
     private InetAddress IP;
     private boolean isTimedOut = false;
     private Calendar calendar;
 
+    /**
+     *
+     * @param username
+     * @param IP
+     */
     public User(String username, InetAddress IP) {
         this.username = username;
         this.IP = IP;
@@ -34,10 +47,17 @@ public class User {
         isTimedOut = timedOut;
     }
 
+    /**
+     *
+     * @return calender time on milliseconds
+     */
     public long getCalendar() {
         return calendar.getTimeInMillis();
     }
 
+    /**
+     * @see #setCalendar() takes the current time on the client
+     */
     public void setCalendar() {
         this.calendar = Calendar.getInstance();
     }
