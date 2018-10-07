16b37a3543ca9f9531817a94f60471cc2a74a503
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-10-07 10:38:57 +0200

    comment on timeout Remover

--- src/TimeoutRemover.java
+++ src/TimeoutRemover.java
@@ -1,27 +1,36 @@
 import java.util.Calendar;
 
+/**
+ * This class is used to remove clients from the servers list of online users if they have not sent an "im alive message"
+ * for 60 seconds.
+ * We use java calender to determent time.
+ * @author Emil, Casper
+ * @version 1.0
+ */
 public class TimeoutRemover implements Runnable{
 
     public void run() {
-        Calendar calendar;
+        Calendar calendar; //instantiate the calender
 
         while (true) {
             try {
-                Thread.sleep(10000);
+                Thread.sleep(10000); //sleeps the thread for 10 seconds.
             } catch (InterruptedException iE) {
                 iE.printStackTrace();
             }
 
-            calendar = Calendar.getInstance();
+            calendar = Calendar.getInstance(); //get instance is the current time past from epohq
 
-            for (User user : Server.users) {
-                if (user.isTimedOut()) {
-                    System.out.println(user + " has timed out!");
-                    Server.users.remove(user);
-                    System.out.println("Updated user list: " + Server.users);
-                    break;
+            for (User user : Server.users) {                                            //looks though the array of users
+                if (user.isTimedOut()) {                                                //if user is timed out
+                    System.out.println(user + " has timed out!");                       //print the username
+                    Server.users.remove(user);                                          //removes that user from the list
+                    System.out.println("Updated user list: " + Server.users);           //prints out the updated userlist
+                    break;                                                              //Stops the for each loop
                 } else if (calendar.getTimeInMillis() - user.getCalendar() > 60000) {
                     user.setTimedOut(true);
+                    // if the time beween the users chech in time and the servers time is greater than 60 seconds
+                    // the user is then set to as have timed out and will gets removed when the thread wakes up.
                 }
             }
         }
